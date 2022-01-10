9bc849c110698bb32870e9b5addd940b8d172960
omg66 <nba.playa66@yahoo.de>
2022-01-10 18:58:16 +0100

    init

---
+++ src/test/java/mg/rat27/cal/CalApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.rat27.cal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
